39f13cb1ab4383eae794426411a031690bcef0bf
MarekWyslucha <marek.wyslucha@gmail.com>
2016-01-16 14:14:18 +0100

    Działający program + profilowanie

--- src/zipbomb/ZipBomb.java
+++ src/zipbomb/ZipBomb.java
@@ -13,21 +13,22 @@ import java.util.logging.Logger;
  * @author Marek Wysłucha
  */
 class BombThread implements Runnable {
+
     private final File file;
     private final int num;
-    
-    public BombThread(int num){
+
+    public BombThread(int num) {
         this.num = num;
         file = new File(System.getProperty("user.dir") + "/bombFile" + num + ".bin");
     }
-    
-    private void createFile(){
-        FileOutputStream fos;
+
+    private void createFile() {
+        FileOutputStream fos = null;
         Random rnd = new Random();
-        byte[] data = new byte[1024*1024];
-        
+        byte[] data = new byte[1024 * 1024];
+
         rnd.nextBytes(data);
-        
+
         try {
             fos = new FileOutputStream(file);
             fos.write(data, 0, data.length);
@@ -36,23 +37,29 @@ class BombThread implements Runnable {
         } catch (FileNotFoundException ex) {
             Logger.getLogger(BombThread.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException ex) {
+
+            }
         }
     }
-    
+
     @Override
     public void run() {
         createFile();
         System.out.println("Wątek " + num + " zakończył działanie.");
     }
-    
+
 }
 
 public class ZipBomb {
 
     public static void main(String[] args) {
-        for(int i=0; i<10; i++){
+        for (int i = 0; i < 10; i++) {
             new BombThread(i).run();
         }
     }
-    
+
 }
